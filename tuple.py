57bcbb453d6d2cbc35e9aaf601e1570c557ef5a9
Brayan1425 <123999442+Brayan1425@users.noreply.github.com>
2024-02-26 20:23:19 -0500

    Ejercicio Informacion tuple.

--- tuple.py
+++ tuple.py
@@ -6,10 +6,4 @@ hermanos =input("cuantos hermanos tienes:")
 informacion = tuple((nombre,edad,altura,hermanos))
 print(f"""La informacion general del usuario es la siguiente : {informacion}""")
 
-education = input("Haz realizado cursos de programacion: (S/N)")
-cuantos_cursos = input("Cuantos cursos de programacion he realizado:")
-cuales_cursos = input("Cuales son los cursos que he realizado:")
 
-experiencia = tuple((education,cuantos_cursos,cuales_cursos))
- 
-print(f"""La experiencia laboral que ha tenido es : {experiencia}""")
